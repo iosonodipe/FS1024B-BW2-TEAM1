bba5f62a84931100818f870ebb81a091ae6e4200
iosonodipe <dipecod@gmail.com>
2024-03-15 15:40:20 +0100

    getalbumsong

--- js/album.js
+++ js/album.js
@@ -60,7 +60,100 @@ function appendCloneAlbumSong(clone) {
   const divTarget = document.querySelector(".target-container-song");
   divTarget.append(clone);
 }
+// function getAlbumSong(album) {
+//   for (let i = 0; i < album.nb_tracks; i++) {
+//     const clone = createCloneAlbumSong();
+//     const track = clone.querySelector(".track-source");
+//     const titoloCanzone = clone.querySelector(".titolo-canzone");
+//     const nomeArtista = clone.querySelector(".artista-nome");
+//     const duration = clone.querySelector(".lunghezza-brano");
+//     const numeroBrano = clone.querySelector(".n-brano");
+
+//     numeroBrano.textContent = i + 1;
+//     let song = document.querySelector('#song')
+//     titoloCanzone.addEventListener("click", () => {
+//       document.querySelector("#song").src = album.tracks.data[i].preview;
+//       song.play()
+//     });
+
+//     titoloCanzone.textContent = album.tracks.data[i].title;
+//     nomeArtista.textContent = album.tracks.data[i].artist.name;
+//     duration.textContent = `${Math.floor(
+//       album.tracks.data[i].duration / 60
+//     )}:${(album.tracks.data[i].duration % 60).toFixed()} min`;
+
+//     appendCloneAlbumSong(clone);
+//   }
+// }
+
 function getAlbumSong(album) {
+
+  //FUNZIONAMENTO BACK AND FORWARD BUTTON//
+  let songs = [];
+  let songIndex = 0;
+  const backBtn = document.querySelector('.backBtn');
+  const forwardBtn = document.querySelector('.forwardBtn');
+  console.log(songs)
+
+  //RIEMPIMENTO ARRAY//
+
+  function array() {
+    album.tracks.data.forEach(element => {
+      let songFn = element.preview
+      songs.push(songFn)
+      return songs;
+    })
+  }
+  array()
+
+  /////////////////////
+
+  function prevSong() {
+    let song = document.querySelector("#song");
+
+    if (songIndex <= 0) {
+      songIndex = songs.length - 1
+    } else {
+      songIndex--
+    }
+
+    song.src = album.tracks.data[songIndex].preview;
+    soundbarArtist(songIndex)
+    song.play()
+
+  };
+  function forwardSong() {
+    let song = document.querySelector("#song");
+
+    if (songIndex >= songs.length - 1) {
+      songIndex = 0
+    } else {
+      songIndex++
+    }
+
+    song.src = album.tracks.data[songIndex].preview;
+    soundbarArtist(songIndex)
+    song.play()
+  }
+
+
+  backBtn.addEventListener('click', prevSong);
+  forwardBtn.addEventListener('click', forwardSong);
+
+
+
+  function soundbarArtist(songIndex){
+    let soundbarImg = document.querySelector("#copertina")
+    let soundbarTitle = document.querySelector(".song")
+    let soundbarArtist = document.querySelector(".artist")
+
+    soundbarImg.src = album.cover_small
+    soundbarTitle.innerText = album.tracks.data[songIndex].title
+    soundbarArtist.innerText = album.contributors[0].name
+  }
+  //////////////////////////////////////////
+  
+
   for (let i = 0; i < album.nb_tracks; i++) {
     const clone = createCloneAlbumSong();
     const track = clone.querySelector(".track-source");
@@ -68,13 +161,30 @@ function getAlbumSong(album) {
     const nomeArtista = clone.querySelector(".artista-nome");
     const duration = clone.querySelector(".lunghezza-brano");
     const numeroBrano = clone.querySelector(".n-brano");
+    
 
     numeroBrano.textContent = i + 1;
     let song = document.querySelector('#song')
     titoloCanzone.addEventListener("click", () => {
       document.querySelector("#song").src = album.tracks.data[i].preview;
+      songIndex = numeroBrano.innerText -1;
+      soundbarArtist(songIndex)
+
       song.play()
+
     });
+
+    
+
+    const playPauseSong = clone.querySelector('.playPauseSong')
+
+    playPauseSong.addEventListener('click', () => {
+      if (playIcon.classList.contains("bi-play-circle-fill")) {
+        playIcon.classList.remove("bi-play-circle-fill");
+        playIcon.classList.add("bi-pause-circle-fill");
+      }
+    });
+
 
     titoloCanzone.textContent = album.tracks.data[i].title;
     nomeArtista.textContent = album.tracks.data[i].artist.name;
@@ -85,6 +195,7 @@ function getAlbumSong(album) {
     appendCloneAlbumSong(clone);
   }
 }
+
 function getAlbumInfo(album) {
   const clone = createCloneAlbum();
   const imageAlbum = clone.querySelector(".album-img");
